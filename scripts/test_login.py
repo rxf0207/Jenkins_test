4416aaf26ee223db7fcdd6bb9b31f1d23504be4c
rxf0207 <Rxf0207@163.com>
2019-06-14 09:50:02 +0800

    第一次上传

---
+++ scripts/test_login.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class TestLogin:
+
+    def test_login1(self):
+        assert 1
+
+    # @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test_login2(self):
+        assert 1
+
+    # @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test_login3(self):
+        assert 0
